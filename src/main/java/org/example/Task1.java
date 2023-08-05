b3959fdec454b11a5928e90481aab56b39ccbe04
Aleksandra <aleksandra.kurek789@gmail.com>
2023-08-05 20:44:58 +0200

    Get the distinct elements sorted in ascending order and making getSortedElements method, create basic measurement information and print the results

--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -1,7 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Task1 {
     public static void main(String[] args) {
@@ -14,6 +13,31 @@ public class Task1 {
         integerList.add(20);
         integerList.add(2);
         integerList.add(5);
+
+        // Get the distinct elements sorted in ascending order
+        List <Integer> sortredElements = getSortedElements(integerList);
+
+        // Basic measurement information
+        int numELementSource = integerList.size();
+        int numSortedElements = sortredElements.size();
+        int minValue = sortredElements.get(0);
+        int maxValue = sortredElements.get(sortredElements.size()-1);
+
+        //Print the results
+        System.out.println(sortredElements);
+        System.out.println("count:" + numELementSource);
+        System.out.println("distinc:" + numSortedElements);
+        System.out.println("min:" + minValue);
+        System.out.println("max:" + maxValue);
+
+
+    }
+
+    private static List<Integer> getSortedElements(List<Integer> integerList) {
+        Set<Integer> sortedElementsSet = new HashSet<>(integerList);
+        List <Integer> sortedElements = new ArrayList<>(sortedElementsSet);
+        Collections.sort(sortedElements);
+        return sortedElements;
 
     }
 }
