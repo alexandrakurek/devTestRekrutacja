06e387cca14db349240bb8616dcd4ad7d7fa132f
Aleksandra <aleksandra.kurek789@gmail.com>
2023-08-05 20:29:43 +0200

    Initial commit

---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        // Simple integerList
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(10);
+        integerList.add(20);
+        integerList.add(20);
+        integerList.add(2);
+        integerList.add(5);
+
+    }
+}
